de97de4c410f9834534d1e7b88b5658d3bcbfa8d
Cristofert Ocampo <ocampocristofert@gmail.com>
2022-11-22 17:44:56 -0500

    Ejercicio terminado

---
+++ randomNumber/app.js
@@ -0,0 +1,19 @@
+let user;
+let number = Math.floor(Math.random() * 11);
+console.log(number);
+
+function guessTheNumber() {
+  for (let count = 0; count < 1; ) {
+    user = prompt("Ingrese un numero del 0 al 10");
+    if (user > number) {
+      alert(`El  numero debe ser menor al ${user}`);
+    }
+    if (user < number) {
+      alert(`El  numero debe ser mayor al ${user}`);
+    } else if (user == number) {
+      alert(`El numero era el ${number} a ganado`);
+      count++;
+    }
+  }
+}
+guessTheNumber();
