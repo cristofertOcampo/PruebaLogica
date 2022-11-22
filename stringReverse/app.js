1f86cf9eeb94f84c9b2b53c1bb9dbd2921f1620b
Cristofert Ocampo <ocampocristofert@gmail.com>
2022-11-22 14:22:03 -0500

    ejercicio finalizado

---
+++ stringReverse/app.js
@@ -0,0 +1,14 @@
+let string ="Hello word";
+
+let phrase="";
+
+function reverse(word){
+  phrase = word.split("");
+  word = phrase.reverse()
+  return phrase = word.join("")
+}
+
+reverse(string)
+console.log(phrase);
+
+// time 20 mins
